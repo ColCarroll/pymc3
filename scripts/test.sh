e5fdb435d089becb6b880b22e2560bd3b5975e41
colin <colin@kensho.com>
2016-12-10 19:20:26 -0500

    bump

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # THEANO_FLAGS='gcc.cxxflags="-march=core2"' nosetests "$@"
-echo $PYTHON_VERSION
-echo $TRAVIS_PYTHON_VERSION
+echo "Python version: $PYTHON_VERSION"
+echo "Travis python version: $TRAVIS_PYTHON_VERSION"
